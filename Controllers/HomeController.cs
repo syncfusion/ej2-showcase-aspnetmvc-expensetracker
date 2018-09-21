712912c46ef836235a9689e2badd6f2f4436bfdf
Piramanayagam <piramanayagamr@syncfusion.com>
2018-09-21 17:58:59 +0530

    feature(EJ2-16372): Publish updated source

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,19 +21,6 @@ namespace EJ2MVCSampleBrowser.Controllers
         {
             ViewBag.startDate = new DateTime(2017, 05, 31);
             ViewBag.endDate = new DateTime(2017, 11, 30);
-            ViewBag.dataLabel = new
-            {
-                name = "x",
-                visible = true,
-                position = "Outside",
-                connectorStyle = new { length = "10%" },
-                font = new
-                {
-                    color = "Black",
-                    size = "14px",
-                    fontFamily = "Roboto"
-                }
-            };
             ViewBag.animation = new { enable = false };
             ViewBag.palettes = new string[] { "#61EFCD", "#CDDE1F", "#FEC200", "#CA765A", "#2485FA", "#F57D7D", "#C152D2",
                     "#8854D9", "#3D4EB8", "#00BCD7" };
@@ -45,30 +32,11 @@ namespace EJ2MVCSampleBrowser.Controllers
             {
                 Width = 0
             };
-            ViewBag.mgLine = new
-            {
-                width = 0
-            };
             ViewBag.content = "<p style='font-family:Roboto;font-size: 16px;font-weight: 400;font-weight: 400;letter-spacing: 0.02em;line-height: 16px;color: #797979 !important;'>Account - Balance</p>";
-            ViewBag.marker = new
-            {
-                visible = true,
-                width = 10,
-                height = 10,
-                fill = "white",
-                border = new { width = 2, color = "#0470D8" },
-            };
             ViewBag.border = new ChartBorder { Width = 0.5, Color = "#0470D8" };
             ViewBag.margin = new ChartMargin { Top = 90 };
             ViewBag.accBalancecontent = "<p style='font-family:Roboto;font-size: 16px;font-weight: 400;font-weight: 400;letter-spacing: 0.02em;line-height: 16px;color: #797979 !important;'>Income - Expense</p>";
             ViewBag.accBalanceborder = new ChartBorder { Width = 0.5, Color = "#A16EE5" };
-            ViewBag.accBalanceMarker = new
-            {
-                visible = true,
-                height = 10,
-                width = 10
-            };
-            ViewBag.accBalanceAnimation = new { enable = false };
             ViewBag.gridToolbar = new object[] { new { text = "Recent Transactions" } };
             return PartialView();
         }
